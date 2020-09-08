9bd94f6202dd52e30caa2f9c13990e215c6234fd
Omar Valencia <omarvalencia9415@gmail.com>
2020-09-08 08:54:01 -0500

    Primer commit

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View,StyleSheet,Text} from 'react-native';
 
-export default function App() {
+const App = () =>{
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>
+        Hello word!!
+      </Text>
     </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  )
+};
+export default App;
